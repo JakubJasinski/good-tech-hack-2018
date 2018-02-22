1c582c0dbcbf507a82bf15050d5c380a6b35ed7b
fgrhlt <simpasp@gmail.com>
2018-02-22 21:24:11 +0100

    Add Sketch files, Add material-ui

--- dashboard-app/src/App.js
+++ dashboard-app/src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AppBar from 'material-ui/AppBar';
+import Chip from 'material-ui/Chip';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <MuiThemeProvider>
+          <div>
+            <AppBar
+              title="Dashboard Application"
+              iconClassNameRight="muidocs-icon-navigation-expand-more"
+            />
+
+            <Chip>
+              Freddan Gillar Chips
+            </Chip>
+          </div>
+        </MuiThemeProvider>
+
         <div className="container">
           <div className="row">
             <div className="col s4 box">1</div>
