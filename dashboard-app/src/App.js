b9f0d1765d8610c38f731e1b7979ab01e97356f0
fgrhlt <simpasp@gmail.com>
2018-02-22 20:27:32 +0100

    Add dashboard react app

---
+++ dashboard-app/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="row">
+            <div className="col s4 box">1</div>
+            <div className="col s4 box">2</div>
+            <div className="col s4 box">3</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
