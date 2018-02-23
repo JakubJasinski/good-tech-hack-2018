f17615d7093b45d9c9e2a08be94db92cfaf150ac
fgrhlt <simpasp@gmail.com>
2018-02-24 00:34:49 +0100

    fixed analytics click menu

--- dashboard-app/src/App.js
+++ dashboard-app/src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import QuestionView from './QuestionView/QuestionView';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+let classNames = require('classnames');
 
 class App extends Component {
 
   componentWillMount() {
     this.setState({
-      currentView: 'questionView'
+      currentView: 'questionView',
+      tab1Active:true,
+      tab2Active:false,
     });
 
     this.views = {
@@ -21,6 +24,15 @@ class App extends Component {
   changeView = (view) => {
     this.setState({currentView:view});
   }
+
+  onMenuClick = (id) => {
+    if(id === 0) {
+      this.setState({tab1Active:true, tab2Active:false, currentView:'questionView'});
+    }
+    if(id === 1) {
+      this.setState({tab1Active:false, tab2Active:true, currentView:'analyticsView'});
+    }
+  }
   render() {
     const theme1 = getMuiTheme({
       slider: {
@@ -30,18 +42,20 @@ class App extends Component {
         handleSizeActive: 26,
       }
     })
+    let menuClass1 = classNames('tab', {'active': this.state.tab1Active});
+    let menuClass2 = classNames('tab', {'active': this.state.tab2Active});
     return (
       <div className="App">
         <MuiThemeProvider muiTheme={theme1}>
           <div>
             <nav className="nav-extended">
              <div className="nav-wrapper">
-               <a href="#" className="brand-logo">Logo</a>
+               <a className="brand-logo">Logo</a>
              </div>
              <div className="nav-content">
                <ul className="tabs tabs-transparent">
-                 <li className="tab active">DIN DAG</li>
-                 <li className="tab">ANALYS</li>
+                 <li className={menuClass1} onClick={() => this.onMenuClick(0)}>DIN DAG</li>
+                 <li className={menuClass2} onClick={() => this.onMenuClick(1)}>ANALYS</li>
                </ul>
              </div>
             </nav>
