7473bc9ec60d6e3aa335efca6432aa5cef9483e3
fgrhlt <simpasp@gmail.com>
2018-02-24 02:29:13 +0100

    fix

--- dashboard-app/src/AnalyticsView/AnalyticsView.js
+++ dashboard-app/src/AnalyticsView/AnalyticsView.js
@@ -3,29 +3,85 @@ import './AnalyticsView.css';
 import CardSwiper from '../CardSwiper/CardSwiper';
 
 class AnalyticsView extends Component {
+
+  componentWillMount() {
+    this.setState({
+      view: true,
+    })
+  }
+  fillContent = () => {
+    let arr = [];
+    if(this.state.view) {
+      this.content1.forEach((field, index) => {
+        arr.push(
+          <div>
+            <div className={field.class} />
+            <p>{field.text}</p>
+          </div>
+        )
+      });
+    }
+    else {
+      this.content2.forEach((field, index) => {
+        arr.push(
+          <div>
+            <div className={field.class} />
+            <p>{field.text}</p>
+          </div>
+        )
+      });
+    }
+    return arr;
+  }
+
+  updateView = () => {
+    let view = this.state.view;
+    this.setState({
+      view: !view,
+    })
+  }
   render() {
-    const content = [
+    const cardContent1 = [
       {text: 'Ta dig lite tid och klura ut vilka arbetsuppgifter som du tycker var roligast och gav dig mest energi i slutet på månaden. Fortsätt med dem!', leftOption: 'Lätt', rightOption:'Svårt'},
       {text: 'Prioritera din sömn! Sömnen har stor betydelse för din generella hälsa då sömnbrist kan resultera i sämre humör, nedsatt immunförsvar och orkeslöshet.', leftOption: 'Lätt', rightOption:'Svårt'},
-      {text: 'Känner du att du kanske är i förstadiet till utmattningssyndrom?', leftOption: 'Lätt', rightOption:'Svårt', button:true},
     ];
+
+    const cardContent2 = [
+      {text: 'Känner du att du kanske är i förstadiet till utmattningssyndrom?', leftOption: 'Lätt', rightOption:'Svårt', button:true},
+
+    ];
+
+    this.content1 = [
+      {text: 'Under slutet av månaden har du haft mer energi',
+        class: 'graph g1'},
+        {text: 'Hela månaden har du hållit dig frisk',
+          class: 'graph g2'},
+          {text: 'Du har haft en oregelbunden sömn',
+            class: 'graph g3'},
+          ];
+    this.content2 = [
+      {text: 'Din energinivå har varit låg genom hela månaden',
+        class: 'graph g4'},
+        {text: 'Ditt hälsotillstånd har varit lågt',
+          class: 'graph g5'},
+          {text: 'Sömnen den här månaden har varit otillräcklig',
+            class: 'graph g6'},
+          ];
+
     return (
-      <div className="analytics-view-container">
+      <div className="analytics-view-container" onClick={() => this.updateView()}>
         <div className="container">
           <h5>Senaste Månaden</h5>
 
           <div className="graph-container">
-            <div className="graph g1"></div>
-            <p>Under slutet av månaden har du haft mer energi</p>
-
-            <div className="graph g2"></div>
-            <p>Hela månaden har du hållit dig frisk</p>
-
-            <div className="graph g3"></div>
-            <p>Du har haft en oregelbunden sömn</p>
+          {this.fillContent()}
           </div>
           <h5 className="bottom">Dina Tips</h5>
-          <CardSwiper content={content} slider={false} smallText={true} />
+          {this.state.view ? (
+            <CardSwiper content={cardContent1} slider={false} smallText={true} key="b1"/>
+          ) : (
+            <CardSwiper content={cardContent2} slider={false} smallText={true} key="b2" index={0} />
+          )}
         </div>
       </div>
     );
