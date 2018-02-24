55755045c48db34e9eeda6fa72119d7c118aa350
fgrhlt <simpasp@gmail.com>
2018-02-24 08:11:54 +0100

    fix

--- dashboard-app/src/Calendar/Calendar.js
+++ dashboard-app/src/Calendar/Calendar.js
@@ -5,11 +5,11 @@ class Calendar extends Component {
   render() {
     return (
       <div className="calendar-container">
-        <div className="calendar-item">To<br /> 21</div>
-        <div className="calendar-item">Fr<br /> 22</div>
-        <div className="calendar-item active">Idag <br />23</div>
-        <div className="calendar-item">Sö<br /> 24</div>
-        <div className="calendar-item">Må<br /> 25</div>
+        <div className="calendar-item">To<br /> 22</div>
+        <div className="calendar-item">Fr<br /> 23</div>
+        <div className="calendar-item active">Idag <br />24</div>
+        <div className="calendar-item">Sö<br /> 25</div>
+        <div className="calendar-item">Må<br /> 26</div>
       </div>
     );
   }
