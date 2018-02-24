a91eb00b6f4136ebde0eae3ac68ca011de37d4db
fgrhlt <simpasp@gmail.com>
2018-02-24 01:20:24 +0100

    fixes

--- dashboard-app/src/AnalyticsView/AnalyticsView.js
+++ dashboard-app/src/AnalyticsView/AnalyticsView.js
@@ -4,6 +4,11 @@ import CardSwiper from '../CardSwiper/CardSwiper';
 
 class AnalyticsView extends Component {
   render() {
+    const content = [
+      {text: 'Ta dig lite tid och klura ut vilka arbetsuppgifter som du tycker var roligast och gav dig mest energi i slutet på månaden. Fortsätt med dem!', leftOption: 'Lätt', rightOption:'Svårt'},
+      {text: 'Prioritera din sömn! Sömnen har stor betydelse för din generella hälsa då sömnbrist kan resultera i sämre humör, nedsatt immunförsvar och orkeslöshet.', leftOption: 'Lätt', rightOption:'Svårt'},
+      {text: 'Känner du att du kanske är i förstadiet till utmattningssyndrom?', leftOption: 'Lätt', rightOption:'Svårt', button:true},
+    ];
     return (
       <div className="analytics-view-container">
         <div className="container">
@@ -11,18 +16,18 @@ class AnalyticsView extends Component {
           <p>Under slutet av månaden har du haft mer energi</p>
 
           <div className="graph-container">
-            <div className="graph"></div>
+            <div className="graph g1"></div>
             <p>Title 1</p>
 
-            <div className="graph"></div>
+            <div className="graph g2"></div>
             <p>Title 2</p>
 
-            <div className="graph"></div>
+            <div className="graph g3"></div>
             <p>Title 3</p>
           </div>
 
           <h5>Dina Tips</h5>
-          <CardSwiper />
+          <CardSwiper content={content} slider={false} smallText={true} />
         </div>
       </div>
     );
